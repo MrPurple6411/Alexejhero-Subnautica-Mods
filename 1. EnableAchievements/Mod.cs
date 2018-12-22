400217d12475c402d5c717923ffd68a4d1ed31d8
AlexejheroYTB <alexejhero@gmail.com>
2018-12-22 16:27:58 +0200

    Updated enableachievements and utility

---
+++ 1. EnableAchievements/Mod.cs
@@ -0,0 +1,46 @@
+﻿using Harmony;
+using ModdingAdventCalendar.Utility;
+using System;
+using System.Reflection;
+
+namespace ModdingAdventCalendar.EnableAchievements
+{
+    public static class QMod
+    {
+        public static void Patch()
+        {
+            try
+            {
+                HarmonyInstance.Create("moddingadventcalendar.enableachievements").PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception e)
+            {
+                Logger.Exception(e, LoggedWhen.Patching);
+            }
+        }
+    }
+
+    public static class Patches
+    {
+        [HarmonyPatch(typeof(GameAchievements), "Unlock")]
+        public static class GameAchievements_Unlock
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(GameAchievements.Id id)
+            {
+                try
+                {
+                    if (typeof(GameAchievements).GetMethod("GetPlatformId", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, new object[] { id }) is string platformId)
+                    {
+                        PlatformUtils.main.GetServices().UnlockAchievement(platformId);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.Exception(e, LoggedWhen.InPatch);
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ Utility/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace ModdingAdventCalendar.Utility
+{
+    public enum LoggedWhen
+    {
+        None = -1,
+        Patching,
+        InPatch,
+    }
+    public class Logger
+    {
+        public static void Exception(Exception e, LoggedWhen loggedWhen = LoggedWhen.None)
+        {
+            string assembly = Assembly.GetCallingAssembly().GetName().FullName;
+            switch (loggedWhen)
+            {
+                case LoggedWhen.Patching:
+                    Console.WriteLine($"[{assembly}] Error while creating harmony patch!");
+                    break;
+                case LoggedWhen.InPatch:
+                    Console.WriteLine($"[{assembly}] Error occured in patched method!");
+                    break;
+                case LoggedWhen.None:
+                default:
+                    break;
+            }
+            Console.WriteLine($"[{assembly}] Exception thrown: {e.Message}");
+            Console.WriteLine($"[{assembly}] Stacktrace: {e.StackTrace}");
+        }
+    }
+}
