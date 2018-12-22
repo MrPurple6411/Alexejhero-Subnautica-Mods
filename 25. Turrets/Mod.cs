f11e357863fb05b270d27f1f40362d29e8c1cab4
AlexejheroYTB <alexejhero@gmail.com>
2018-12-22 16:35:37 +0200

    Added turrets

---
+++ 25. Turrets/Mod.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using ModdingAdventCalendar.Utility;
+using System;
+using System.Reflection;
+
+namespace ModdingAdventCalendar.Turrets
+{
+    public static class QMod
+    {
+        public static void Patch()
+        {
+            try
+            {
+                HarmonyInstance.Create("moddingadventcalendar.turrets").PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception e)
+            {
+                Logger.Exception(e, LoggedWhen.Patching);
+            }
+        }
+    }
+}
