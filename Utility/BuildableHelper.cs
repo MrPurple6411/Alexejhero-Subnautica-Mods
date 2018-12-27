72e41d0d2b0eaef7838fc29b57c9fac6c7f598d2
AlexejheroYTB <alexejhero@gmail.com>
2018-12-27 17:30:13 +0200

    Create BuildableHelper.cs

---
+++ Utility/BuildableHelper.cs
@@ -0,0 +1,29 @@
+﻿using SMLHelper.V2.Handlers;
+using System;
+using UnityEngine;
+
+namespace ModdingAdventCalendar.Utility
+{
+    public class BuildableHelper
+    {
+        public TechType techType;
+
+        public BuildableHelper(string id, string displayname, string tooltip, TechGroup group, TechCategory category, TechType after = TechType.None, bool unlockonstart = true, Atlas.Sprite atlasSprite = null, Sprite unitySprite = null)
+        {
+            if (atlasSprite != null && unitySprite != null) throw new ArgumentException("Both atlasSprite and unitySprite are not null!");
+
+            if (atlasSprite != null) techType = TechTypeHandler.AddTechType(id, displayname, tooltip, atlasSprite, unlockonstart);
+            else if (unitySprite != null) techType = TechTypeHandler.AddTechType(id, displayname, tooltip, unitySprite, unlockonstart);
+            else techType = TechTypeHandler.AddTechType(id, displayname, tooltip, unlockonstart);
+
+            CraftDataHandler.AddBuildable(techType);
+            if (after == TechType.None) CraftDataHandler.AddToGroup(group, category, techType);
+            else CraftDataHandler.AddToGroup(group, category, techType, after);
+        }
+
+        public static implicit operator TechType(BuildableHelper bh)
+        {
+            return bh.techType;
+        }
+    }
+}
