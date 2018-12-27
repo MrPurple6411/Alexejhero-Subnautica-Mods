e83e2523aa4014ab1de44890e32dfc761625f4bd
AlexejheroYTB <alexejhero@gmail.com>
2018-12-27 17:19:36 +0200

    Create PrefabHelper.cs

---
+++ Utility/PrefabHelper.cs
@@ -0,0 +1,21 @@
+﻿using SMLHelper.V2.Assets;
+using System;
+using UnityEngine;
+
+namespace ModdingAdventCalendar.Utility
+{
+    public class PrefabHelper : ModPrefab
+    {
+        public Func<GameObject> Function;
+
+        public PrefabHelper(string classid, string prefabfilename, TechType techtype, Func<GameObject> function) : base(classid, prefabfilename, techtype)
+        {
+            Function = function;
+        }
+
+        public override GameObject GetGameObject()
+        {
+            return Function.Invoke();
+        }
+    }
+}
