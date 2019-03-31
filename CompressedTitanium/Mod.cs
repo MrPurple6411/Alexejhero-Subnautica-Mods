4e9b2b0adea8673f8db59d3240416ff97668ad98
AlexejheroYTB <alexejhero@gmail.com>
2019-03-31 15:33:01 +0300

    a

---
+++ CompressedTitanium/Mod.cs
@@ -0,0 +1,246 @@
+﻿using Harmony;
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Utility;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace AlexejheroYTB.CompressedTitanium
+{
+    public static class Mod
+    {
+        public static bool Patched = false;
+
+        public static ModCraftTreeLinkingNode CompressTab;
+        public static ModCraftTreeLinkingNode DecompressTab;
+
+        public static readonly TechType TI_1 = TechType.Titanium;
+        public static readonly TechType TI_10 = TechType.TitaniumIngot;
+        public static TechType TI_100;
+        public static TechType TI_1000;
+        public static TechType TI_10000;
+        public static TechType TI_100000;
+        public static TechType TI_1000000;
+
+        public static readonly Atlas.Sprite TI_1_Sprite = SpriteManager.Get(TechType.Titanium);
+        public static readonly Atlas.Sprite TI_10_Sprite = SpriteManager.Get(TechType.TitaniumIngot);
+        public static readonly Atlas.Sprite TI_100_Sprite = ImageUtils.LoadSpriteFromFile(Path.Combine(Assembly.GetExecutingAssembly().Location, "../Assets/compressed.png"));
+        public static readonly Atlas.Sprite TI_1000_Sprite = ImageUtils.LoadSpriteFromFile(Path.Combine(Assembly.GetExecutingAssembly().Location, "../Assets/doublecompressed.png"));
+        public static readonly Atlas.Sprite TI_10000_Sprite = ImageUtils.LoadSpriteFromFile(Path.Combine(Assembly.GetExecutingAssembly().Location, "../Assets/triplecompressed.png"));
+        public static readonly Atlas.Sprite TI_100000_Sprite = ImageUtils.LoadSpriteFromFile(Path.Combine(Assembly.GetExecutingAssembly().Location, "../Assets/quadruplecompressed.png"));
+        public static readonly Atlas.Sprite TI_1000000_Sprite = ImageUtils.LoadSpriteFromFile(Path.Combine(Assembly.GetExecutingAssembly().Location, "../Assets/singularitycompressed.png"));
+
+        public static void Patch()
+        {
+            if (Patched) return;
+            Patched = true;
+
+            LoadFabricator();
+            OverrideDefaults();
+            LoadItems();
+        }
+
+        public static void LoadFabricator()
+        {
+            ModCraftTreeRoot Root = CraftTreeHandler.CreateCustomCraftTreeAndType("compressor", out CraftTree.Type Compressor);
+            
+            if (DefabricatorPresent())
+            {
+                CompressTab = Root;
+                DecompressTab = null;
+            }
+            else
+            {
+                CompressTab = Root.AddTabNode("CompressedTitaniumCompress", "Compress", TI_1000000_Sprite);
+                DecompressTab = Root.AddTabNode("CompressedTitaniumDecompress", "Decompress", TI_1_Sprite);
+            }
+
+            new Fabricator().Patch(Compressor);
+        }
+
+        public static void OverrideDefaults()
+        {
+            LanguageHandler.SetTechTypeTooltip(TI_1, "1 Titanium");
+            LanguageHandler.SetTechTypeTooltip(TI_10, "10 Titanium");
+
+            CraftTreeHandler.RemoveNode(CraftTree.Type.Fabricator, "Resources", "BasicMaterials", "TitaniumIngot");
+            CompressTab.AddCraftingNode(TI_10);
+
+            TechType TI_10_Decompress = TechTypeHandler.AddTechType("titaniumIngotDecompress", "Titanium", "1 Titanium", TI_1_Sprite);
+            TechData TI_10_Decompress_TechData = new TechData(new Ingredient(TI_10, 1)) { craftAmount = 0, LinkedItems = TI_1.Repeat(10) };
+            CraftDataHandler.SetTechData(TI_10_Decompress, TI_10_Decompress_TechData);
+            DecompressTab?.AddCraftingNode(TI_10_Decompress);
+        }
+
+        public static void LoadItems()
+        {
+            #region Compressed Titanium Ingot
+
+            TI_100 = TechTypeHandler.AddTechType($"compressedTitaniumIngot", "Compressed Titanium Ingot", "100 Titanium", TI_100_Sprite);
+            TechData TI_100_TechData = new TechData(new Ingredient(TI_10, 10));
+            CraftDataHandler.SetTechData(TI_100, TI_100_TechData);
+            CraftDataHandler.SetCraftingTime(TI_100, 10);
+
+            TechType TI_100_Decompress = TechTypeHandler.AddTechType("compressedTitaniumIngotDecompress", "Titanium Ingot", "10 Titanium", TI_10_Sprite);
+            TechData TI_100_Decompress_TechData = new TechData(new Ingredient(TI_100, 1)) { craftAmount = 0, LinkedItems = TI_10.Repeat(10) };
+            CraftDataHandler.SetTechData(TI_100_Decompress, TI_100_Decompress_TechData);
+            CraftDataHandler.SetCraftingTime(TI_100_Decompress, 10);
+
+            #endregion
+
+            #region Double Compressed Titanium Ingot
+
+            TI_1000 = TechTypeHandler.AddTechType($"doubleCompressedTitaniumIngot", "Double Compressed Titanium Ingot", "1.000 Titanium", TI_1000_Sprite);
+            TechData TI_1000_TechData = new TechData(new Ingredient(TI_100, 10));
+            CraftDataHandler.SetTechData(TI_1000, TI_1000_TechData);
+            CraftDataHandler.SetCraftingTime(TI_1000, 15);
+
+            TechType TI_1000_Decompress = TechTypeHandler.AddTechType("doubleCompressedTitaniumIngotDecompress", "Compressed Titanium Ingot", "100 Titanium", TI_100_Sprite);
+            TechData TI_1000_Decompress_TechData = new TechData(new Ingredient(TI_1000, 1)) { craftAmount = 0, LinkedItems = TI_100.Repeat(10) };
+            CraftDataHandler.SetTechData(TI_1000_Decompress, TI_1000_Decompress_TechData);
+            CraftDataHandler.SetCraftingTime(TI_1000_Decompress, 15);
+
+            #endregion
+
+            #region Triple Compressed Titanium Ingot
+
+            TI_10000 = TechTypeHandler.AddTechType($"tripleCompressedTitaniumIngot", "Triple Compressed Titanium Ingot", "10.000 Titanium", TI_10000_Sprite);
+            TechData TI_10000_TechData = new TechData(new Ingredient(TI_1000, 10));
+            CraftDataHandler.SetTechData(TI_10000, TI_10000_TechData);
+            CraftDataHandler.SetCraftingTime(TI_10000, 20);
+
+            TechType TI_10000_Decompress = TechTypeHandler.AddTechType("tripleCompressedTitaniumIngotDecompress", "Double Compressed Titanium Ingot", "1.000 Titanium", TI_1000_Sprite);
+            TechData TI_10000_Decompress_TechData = new TechData(new Ingredient(TI_10000, 1)) { craftAmount = 0, LinkedItems = TI_1000.Repeat(10) };
+            CraftDataHandler.SetTechData(TI_10000_Decompress, TI_10000_Decompress_TechData);
+            CraftDataHandler.SetCraftingTime(TI_10000_Decompress, 20);
+
+            #endregion
+
+            #region Quadruple Compressed Titanium Ingot
+
+            TI_100000 = TechTypeHandler.AddTechType($"quadrupleCompressedTitaniumIngot", "Quadruple Compressed Titanium Ingot", "100.000 Titanium", TI_100000_Sprite);
+            TechData TI_100000_TechData = new TechData(new Ingredient(TI_10000, 10));
+            CraftDataHandler.SetTechData(TI_100000, TI_100000_TechData);
+            CraftDataHandler.SetCraftingTime(TI_100000, 25);
+
+            TechType TI_100000_Decompress = TechTypeHandler.AddTechType("quadrupleCompressedTitaniumIngotDecompress", "Triple Compressed Titanium Ingot", "10.000 Titanium", TI_10000_Sprite);
+            TechData TI_100000_Decompress_TechData = new TechData(new Ingredient(TI_100000, 1)) { craftAmount = 0, LinkedItems = TI_10000.Repeat(10) };
+            CraftDataHandler.SetTechData(TI_100000_Decompress, TI_100000_Decompress_TechData);
+            CraftDataHandler.SetCraftingTime(TI_100000_Decompress, 25);
+
+            #endregion
+
+            #region Titanium Ingot Singularity
+
+            TI_1000000 = TechTypeHandler.AddTechType($"singularityCompressedTitaniumIngot", "Titanium Ingot Singularity", "1.000.000 Titanium", TI_1000000_Sprite);
+            TechData TI_1000000_TechData = new TechData(new Ingredient(TI_100000, 10));
+            CraftDataHandler.SetTechData(TI_1000000, TI_1000000_TechData);
+            CraftDataHandler.SetCraftingTime(TI_1000000, 30);
+
+            TechType TI_1000000_Decompress = TechTypeHandler.AddTechType("singularityCompressedTitaniumIngotDecompress", "Quadruple Compressed Titanium Ingot", "100.000 Titanium", TI_100000_Sprite);
+            TechData TI_1000000_Decompress_TechData = new TechData(new Ingredient(TI_1000000, 1)) { craftAmount = 0, LinkedItems = TI_100000.Repeat(10) };
+            CraftDataHandler.SetTechData(TI_1000000_Decompress, TI_1000000_Decompress_TechData);
+            CraftDataHandler.SetCraftingTime(TI_1000000_Decompress, 30);
+
+            #endregion
+
+            CompressTab.AddCraftingNode(TI_100, TI_1000, TI_10000, TI_100000, TI_1000000);
+            DecompressTab?.AddCraftingNode(TI_100_Decompress, TI_1000_Decompress, TI_10000_Decompress, TI_100000_Decompress, TI_1000000_Decompress);
+        }
+
+        public static bool DefabricatorPresent()
+        {
+            foreach (Assembly a in AppDomain.CurrentDomain.GetAssemblies())
+                if (a.GetName().Name == "AgonyDefabricator") return true;
+            return false;
+        }
+    }
+
+    public class Fabricator : ModPrefab
+    {
+        public CraftTree.Type TreeTypeID { get; set; }
+
+        public Fabricator() : base("CompressorFab", $"CompressorFabPrefab", TechTypeHandler.AddTechType("CompressorFabTT", "Compressor", "A machine which allows you to compress resources.\n\nIf PVD's \"Defabricator\" mod is not present, you can also decompress items using this fabricator."))
+        {
+        }
+
+        public void Patch(CraftTree.Type craftTreeType)
+        {
+            TreeTypeID = craftTreeType;
+
+            CraftDataHandler.SetTechData(TechType, new TechData()
+            {
+                Ingredients = new List<Ingredient>
+                {
+                    new Ingredient(TechType.Titanium, 2),
+                    new Ingredient(TechType.ComputerChip, 1),
+                    new Ingredient(TechType.Diamond, 1),
+                    new Ingredient(TechType.Lead, 1),
+                }
+            });
+
+            CraftDataHandler.AddBuildable(TechType);
+            CraftDataHandler.AddToGroup(TechGroup.InteriorModules, TechCategory.InteriorModule, TechType, TechType.Fabricator);
+
+            LanguageHandler.SetLanguageLine("UseCompressor", "Use Vehicle Module Fabricator");
+
+            PrefabHandler.RegisterPrefab(this);
+        }
+
+        public override GameObject GetGameObject()
+        {
+            var prefab = GameObject.Instantiate(Resources.Load<GameObject>("Submarine/Build/CyclopsFabricator"));
+
+            GameObject fabLight = prefab.FindChild("fabricatorLight");
+            GameObject fabModel = prefab.FindChild("submarine_fabricator_03");
+
+            PrefabIdentifier prefabId = prefab.AddComponent<PrefabIdentifier>();
+            prefabId.ClassId = "CompressorFab";
+            prefabId.name = "Compressor";
+
+            prefab.AddComponent<TechTag>().type = TechType;
+
+            fabModel.transform.localPosition = new Vector3(fabModel.transform.localPosition.x, fabModel.transform.localPosition.y - 0.8f, fabModel.transform.localPosition.z);
+            fabLight.transform.localPosition = new Vector3(fabLight.transform.localPosition.x, fabLight.transform.localPosition.y - 0.8f, fabLight.transform.localPosition.z);
+
+            SkyApplier skyApplier = prefab.GetComponent<SkyApplier>();
+            skyApplier.renderers = prefab.GetComponentsInChildren<Renderer>();
+            skyApplier.anchorSky = Skies.Auto;
+
+            global::Fabricator fabricator = prefab.GetComponent<global::Fabricator>();
+            fabricator.craftTree = TreeTypeID;
+            fabricator.handOverText = "Use Compressor";
+
+            fabricator.GetComponent<GhostCrafter>().SetInstanceField("powerRelay", new PowerRelay());
+
+            Constructable constructible = prefab.AddComponent<Constructable>();
+            constructible.allowedInBase = true;
+            constructible.allowedInSub = true;
+            constructible.allowedOutside = false;
+            constructible.allowedOnCeiling = false;
+            constructible.allowedOnGround = false;
+            constructible.allowedOnWall = true;
+            constructible.allowedOnConstructables = false;
+            constructible.controlModelState = true;
+            constructible.rotationEnabled = false;
+            constructible.techType = TechType; // This was necessary to correctly associate the recipe at building time
+            constructible.model = fabModel;
+
+            return prefab;
+        }
+    }
+
+    public static class Extensions
+    {
+        public static List<T> Repeat<T>(this T obj, int c)
+        {
+            List<T> list = new List<T>();
+            for (int i = 0; i < c; i++) list.Add(obj);
+            return list;
+        }
+    }
+}
--- SonyWalkman/Mod.cs
+++ SonyWalkman/Mod.cs
@@ -1,7 +1,10 @@
 ﻿using AlexejheroYTB.Common;
+using Harmony;
 using MP3player;
 using SMLHelper.V2.Assets;
 using SMLHelper.V2.Crafting;
+using SMLHelper.V2.Handlers;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -16,29 +19,21 @@ namespace AlexejheroYTB.Radio
         public static void Patch()
         {
             HarmonyHelper.Patch();
-            new SonyWalkman().Patch();
+            new Radio().Patch();
+            ControlItems.Patch();
         }
     }
 
-    public static class Language
-    {
-        public const string id = "mp3playerradio";
-        public const string displayName = "MP3 Player";
-        public const string tooltip = "Can play music from the \"OST\" folder.\nI recommend Abandon Ship.";
-        public const string leftClickTooltip = "play/pause";
-        public const string middleClickTooltip = "open/close interface";
-    }
-
-    public class SonyWalkman : Craftable
+    public class Radio : Craftable
     {
         public new void Patch()
         {
             base.Patch();
-            ItemActionHelper.RegisterAction(MouseButton.Left, TechType, OnLeftClick, Language.leftClickTooltip, (item) => !item.item.gameObject.GetComponent<OSTAudioPlayer>().IsOpen);
-            ItemActionHelper.RegisterAction(MouseButton.Middle, TechType, OnMiddleClick, Language.middleClickTooltip, true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, TechType, OnLeftClick, "play/pause", (item) => !item.item.gameObject.GetComponent<OSTAudioPlayer>().IsOpen);
+            ItemActionHelper.RegisterAction(MouseButton.Middle, TechType, OnMiddleClick, "open/close interface", true.ToPredicate<InventoryItem>());
         }
 
-        public SonyWalkman() : base(Language.id, Language.displayName, Language.tooltip) { }
+        public Radio() : base("MP3Player_Radio", "MP3 Player", "Can play music from the \"OST\" folder.\nI recommend Abandon Ship.") { }
 
         protected override TechData GetBlueprintRecipe() => new TechData() { craftAmount = 1 };
         public override CraftTree.Type FabricatorType => CraftTree.Type.Fabricator;
@@ -60,9 +55,9 @@ namespace AlexejheroYTB.Radio
 
             Object.DestroyImmediate(obj.GetComponent<Battery>());
 
-            OSTAudioPlayer radio = obj.AddComponent<OSTAudioPlayer>();
+            OSTAudioPlayer player = obj.AddComponent<OSTAudioPlayer>();
 
-            PseudoStorage storage = obj.AddComponent<PseudoStorage>();
+            obj.AddComponent<PseudoStorage>().player = player;
 
             return obj;
         }
@@ -147,20 +142,175 @@ namespace AlexejheroYTB.Radio
 
     public class PseudoStorage : StorageContainer
     {
+        public OSTAudioPlayer player;
+
         public override void Awake()
         {
             GameObject pseudoStorage = new GameObject("PseudoStorage");
             pseudoStorage.transform.SetParent(transform);
 
-            height = 4;
-            width = 6;
+            height = 6;
+            width = 8;
             storageLabel = "MP3 PLAYER";
             storageRoot = pseudoStorage.AddComponent<ChildObjectIdentifier>();
 
             base.Awake();
 
+            container.containerType = (ItemsContainerType)581;
+            container.Clear();
+
+            Pickupable _1_Previous = CraftData.InstantiateFromPrefab(ControlItems._1_Previous).GetComponentInChildren<Pickupable>();
+            Pickupable _2_1_Play = CraftData.InstantiateFromPrefab(ControlItems._2_1_Play).GetComponentInChildren<Pickupable>();
+            Pickupable _3_Next = CraftData.InstantiateFromPrefab(ControlItems._3_Next).GetComponentInChildren<Pickupable>();
+            Pickupable _4_1_EnableRepeat = CraftData.InstantiateFromPrefab(ControlItems._4_1_EnableRepeat).GetComponentInChildren<Pickupable>();
+            Pickupable _5_1_EnableShuffle = CraftData.InstantiateFromPrefab(ControlItems._5_1_EnableShuffle).GetComponentInChildren<Pickupable>();
+            Pickupable _6_ExitInterface = CraftData.InstantiateFromPrefab(ControlItems._6_ExitInterface).GetComponentInChildren<Pickupable>();
+
+            ControlItems.CorrespondingAudioPlayers.Add(_1_Previous, player);
+            ControlItems.CorrespondingAudioPlayers.Add(_2_1_Play, player);
+            ControlItems.CorrespondingAudioPlayers.Add(_3_Next, player);
+            ControlItems.CorrespondingAudioPlayers.Add(_4_1_EnableRepeat, player);
+            ControlItems.CorrespondingAudioPlayers.Add(_5_1_EnableShuffle, player);
+            ControlItems.CorrespondingAudioPlayers.Add(_6_ExitInterface, player);
+
+            container.AddItem(_1_Previous);
+            container.AddItem(_2_1_Play);
+            container.AddItem(_3_Next);
+            container.AddItem(_4_1_EnableRepeat);
+            container.AddItem(_5_1_EnableShuffle);
+            container.AddItem(_6_ExitInterface);
+
             container.isAllowedToAdd = new IsAllowedToAdd((p, b) => false);
             container.isAllowedToRemove = new IsAllowedToRemove((p, b) => false);
+        }
+    }
+
+    public static class ControlItems
+    {
+        public static TechType _1_Previous;
+        public static TechType _2_1_Play;
+        public static TechType _2_2_Pause;
+        public static TechType _3_Next;
+        public static TechType _4_1_EnableRepeat;
+        public static TechType _4_2_DisableRepeat;
+        public static TechType _5_1_EnableShuffle;
+        public static TechType _5_2_DisableShuffle;
+        public static TechType _6_ExitInterface;
+
+        public static Dictionary<Pickupable, OSTAudioPlayer> CorrespondingAudioPlayers = new Dictionary<Pickupable, OSTAudioPlayer>();
+
+        public static void Patch()
+        {
+            AddTechTypes();
+            SetTechTypeProperties();
+            AddActions();
+        }
+
+        public static void AddTechTypes()
+        {
+            _1_Previous = TechTypeHandler.AddTechType("MP3Player_Controls_Previous", "Previous", "Previous song");
+            _2_1_Play = TechTypeHandler.AddTechType("MP3Player_Controls_Play", "Play/Pause", "Current status: <color=#C21807FF>paused</color>");
+            _2_2_Pause = TechTypeHandler.AddTechType("MP3Player_Controls_Pause", "Pause/Pause", "Current status: <color=#4CBB17FF>playing</color>");
+            _3_Next = TechTypeHandler.AddTechType("MP3Player_Controls_Next", "Next", "Next song");
+            _4_1_EnableRepeat = TechTypeHandler.AddTechType("MP3Player_Controls_EnableRepeat", "Repeat", "Current status: <color=#C21807FF>disabled</color>");
+            _4_2_DisableRepeat = TechTypeHandler.AddTechType("MP3Player_Controls_DisableRepeat", "Repeat", "Current status: <color=#4CBB17FF>enabled</color>");
+            _5_1_EnableShuffle = TechTypeHandler.AddTechType("MP3Player_Controls_EnableShuffle", "Shuffle", "Current status: <color=#C21807FF>disabled</color>");
+            _5_2_DisableShuffle = TechTypeHandler.AddTechType("MP3Player_Controls_DisableShuffle", "Shuffle", "Current status: <color=#4CBB17FF>enabled</color>");
+            _6_ExitInterface = TechTypeHandler.AddTechType("MP3Player_Controls_ExitInterface", "Exit", "Close the interface");
+        }
+        public static void SetTechTypeProperties()
+        {
+            CraftDataHandler.SetItemSize(_1_Previous, 2, 2);
+            CraftDataHandler.SetItemSize(_2_1_Play, 2, 2);
+            CraftDataHandler.SetItemSize(_2_2_Pause, 2, 2);
+            CraftDataHandler.SetItemSize(_3_Next, 2, 2);
+            CraftDataHandler.SetItemSize(_4_1_EnableRepeat, 2, 2);
+            CraftDataHandler.SetItemSize(_4_2_DisableRepeat, 2, 2);
+            CraftDataHandler.SetItemSize(_5_1_EnableShuffle, 2, 2);
+            CraftDataHandler.SetItemSize(_5_2_DisableShuffle, 2, 2);
+            CraftDataHandler.SetItemSize(_6_ExitInterface, 2, 2);
+        }
+        public static void AddActions()
+        {
+            ItemActionHelper.RegisterAction(MouseButton.Left, _1_Previous, _1_Previous_OnClick, "Use", true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, _2_1_Play, _2_1_Play_OnClick, "Use", true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, _2_2_Pause, _2_2_Pause_OnClick, "Use", true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, _3_Next, _3_Next_OnClick, "Use", true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, _4_1_EnableRepeat, _4_1_EnableRepeat_OnClick, "Use", true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, _4_2_DisableRepeat, _4_2_DisableRepeat_OnClick, "Use", true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, _5_1_EnableShuffle, _5_1_EnableShuffle_OnClick, "Use", true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, _5_2_DisableShuffle, _5_2_DisableShuffle_OnClick, "Use", true.ToPredicate<InventoryItem>());
+            ItemActionHelper.RegisterAction(MouseButton.Left, _6_ExitInterface, _6_ExitInterface_OnClick, "Use", true.ToPredicate<InventoryItem>());
+        }
+
+        #region Actions
+
+        public static void _1_Previous_OnClick(InventoryItem item)
+        {
+
+        }
+        public static void _2_1_Play_OnClick(InventoryItem item)
+        {
+
+        }
+        public static void _2_2_Pause_OnClick(InventoryItem item)
+        {
+
+        }
+        public static void _3_Next_OnClick(InventoryItem item)
+        {
+
+        }
+        public static void _4_1_EnableRepeat_OnClick(InventoryItem item)
+        {
+
+        }
+        public static void _4_2_DisableRepeat_OnClick(InventoryItem item)
+        {
+
+        }
+        public static void _5_1_EnableShuffle_OnClick(InventoryItem item)
+        {
+
+        }
+        public static void _5_2_DisableShuffle_OnClick(InventoryItem item)
+        {
+
+        }
+        public static void _6_ExitInterface_OnClick(InventoryItem item)
+        {
+            Object.FindObjectsOfType<OSTAudioPlayer>().Do(p => p.IsOpen = false);
+            //CorrespondingAudioPlayers[item.item].IsOpen = false;
+            Player.main.GetPDA().Close();
+            Player.main.GetPDA().Open(PDATab.Inventory);
+        }
+
+        #endregion
+    }
+
+    public static class Patches
+    {
+        [HarmonyPatch(typeof(ItemsContainer))]
+        [HarmonyPatch("IItemsContainer.UpdateContainer")]
+        public static class ItemsContainer_IItemsContainer_UpdateContainer
+        {
+            [HarmonyPrefix]
+            public static bool Prefix(ItemsContainer __instance)
+            {
+                if (__instance.containerType == (ItemsContainerType)581) return false;
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(PDA))]
+        [HarmonyPatch("Close")]
+        public static class PDA_Close
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                Object.FindObjectsOfType<OSTAudioPlayer>().Do(p => p.IsOpen = false);
+            }
         }
     }
 }
