d1d1772367dbc16e25b5db929750c0707b144c96
AlexejheroYTB <alexejhero@gmail.com>
2020-01-30 16:10:56 +0200

    Add horizontal wall lockers

---
+++ HorizontalWallLockers/Mod.cs
@@ -0,0 +1,42 @@
+﻿using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+using QModManager.API.ModLoading;
+using UnityEngine;
+using Logger = AlexejheroYTB.Common.Logger;
+using System;
+
+namespace AlexejheroYTB.HorizontalWallLockers
+{
+    [QModCore]
+    public static class QMod
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            new HorizontalWallLocker().Patch();
+            Logger.Log("Patched");
+        }
+    }
+
+    public class HorizontalWallLocker : Buildable
+    {
+        public HorizontalWallLocker() : base("horizontalwalllocker", "Horizontal Wall Locker", "5×6 wall-mounted storage solution.") { }
+
+        public override TechGroup GroupForPDA => TechGroup.InteriorModules;
+        public override TechCategory CategoryForPDA => TechCategory.InteriorModule;
+
+        public override GameObject GetGameObject()
+        {
+            GameObject prefab = CraftData.GetPrefabForTechType(TechType.SmallLocker);
+            prefab.transform.Rotate(180, 180, 180);
+
+            GameObject obj = GameObject.Instantiate(prefab);
+            return obj;
+        }
+
+        protected override TechData GetBlueprintRecipe()
+        {
+            return new TechData(new Ingredient(TechType.Titanium, 2));
+        }
+    }
+}
