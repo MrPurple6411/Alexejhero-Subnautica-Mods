d6ed6470d927727dc996043153d55aa16f3c0fe4
MrPurple6411 <mrpurple6411@gmail.com>
2023-11-10 11:24:12 -0700

    Fix config menu's not showing up and incorrect order

--- ConfigurableDrillableCount/Config.cs
+++ ConfigurableDrillableCount/Config.cs
@@ -7,10 +7,10 @@ using Nautilus.Options.Attributes;
 [Menu(MyPluginInfo.PLUGIN_NAME, LoadOn = MenuAttribute.LoadEvents.MenuOpened, SaveOn = MenuAttribute.SaveEvents.ChangeValue|MenuAttribute.SaveEvents.SaveGame)]
 public class Config : ConfigFile
 {
-    [Slider("Minimum", 1, 100)]
+    [Slider("Minimum", 1, 100, Step = 1, Order = 1)]
     public int min = 1;
 
-    [Slider("Maximum", 1, 100)]
+    [Slider("Maximum", 1, 100, Step = 1, Order = 0)]
     public int max = 3;
 
     public static Config Instance { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
